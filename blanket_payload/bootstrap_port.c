55df3df8b9f09679b3481495c0fa85a5ffafb676
Brandon Azad <bazad@cs.stanford.edu>
2018-05-03 20:58:56 -0700

    Fix bootstrap port initialization in the payload
    The bootstrap port was not being properly initialized in the payload,
    causing commands like launchctl to fail when run in the spawned shell.
    Now that the bootstrap port is properly initialized, launchctl works as
    expected.

--- blanket_payload/bootstrap_port.c
+++ blanket_payload/bootstrap_port.c
@@ -1,7 +1,19 @@
+/*
+ * bootstrap_port.c
+ * Brandon Azad
+ *
+ * When spawning a process on iOS 11 by calling posix_spawn() in launchd, the bootstrap state does
+ * not get initialized properly. Including this file will re-initialize the bootstrap state so that
+ * interaction with launchd works as expected.
+ *
+ */
+
 #include <mach/mach.h>
 
-#include "headers/bootstrap.h"
-#include "headers/mach_vm.h"
+// Definitions and prototypes for XPC.
+typedef void *xpc_pipe_t;
+extern xpc_pipe_t xpc_pipe_create_from_port(mach_port_t port, uint32_t flags);
+extern void xpc_release(void *);
 
 // Look up the bootstrap port so that we can communicate with launchd.
 static mach_port_t
@@ -20,16 +32,47 @@ lookup_bootstrap_port() {
 		mach_port_deallocate(mach_task_self(), init_ports[i]);
 	}
 	// Deallocate the memory.
-	mach_vm_deallocate(mach_task_self(), (mach_vm_address_t)init_ports,
+	vm_deallocate(mach_task_self(), (vm_address_t)init_ports,
 			init_port_count * sizeof(*init_ports));
 	// Return the bootstrap port.
 	return bootstrap;
 }
 
-// Look up the bootstrap port during initialization. I'm not sure why this isn't happening
-// automatically.
+// Set up our new bootstrap port.
+static void
+setup_bootstrap_port() {
+	task_set_special_port(mach_task_self(), TASK_BOOTSTRAP_PORT, bootstrap_port);
+}
+
+// Initialize the XPC bootstrap pipe.
+static void
+setup_xpc_bootstrap_pipe() {
+	struct _os_alloc_once_s {
+		long once;
+		void *ptr;
+	};
+	extern struct _os_alloc_once_s _os_alloc_once_table[];
+	struct xpc_global_data {
+		uint64_t a;
+		uint64_t xpc_flags;
+		mach_port_t task_bootstrap_port;
+		xpc_pipe_t xpc_bootstrap_pipe;
+	};
+	struct xpc_global_data *xpc_global_data = _os_alloc_once_table[1].ptr;
+	xpc_global_data->task_bootstrap_port = bootstrap_port;
+	xpc_pipe_t old_pipe = xpc_global_data->xpc_bootstrap_pipe;
+	xpc_global_data->xpc_bootstrap_pipe = xpc_pipe_create_from_port(bootstrap_port, 0);
+	xpc_release(old_pipe);
+}
+
+// Initialize the bootstrap port and create a new XPC bootstrap pipe during initialization. This
+// happens automatically for processes that launchd spawns the normal way.
 __attribute__((constructor))
 static void
 init_bootstrap_port() {
-	bootstrap_port = lookup_bootstrap_port();
+	if (bootstrap_port == MACH_PORT_NULL) {
+		bootstrap_port = lookup_bootstrap_port();
+		setup_bootstrap_port();
+		setup_xpc_bootstrap_pipe();
+	}
 }
